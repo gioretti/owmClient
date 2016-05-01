259fbf8e50811495db3153ad38e60fabf3b8d316
moretgio <moretgio@students.zhaw.ch>
2016-05-01 16:50:57 +0200

    Forecast16 most importand data

--- lib/src/main/java/org/bitpipeline/lib/owm/Forecast16WeatherData.java
+++ lib/src/main/java/org/bitpipeline/lib/owm/Forecast16WeatherData.java
@@ -1,25 +1,25 @@
 package org.bitpipeline.lib.owm;
 
-import org.bitpipeline.lib.owm.WeatherData.Main;
 import org.json.JSONObject;
 
-public class Forecast16WeatherData extends LocalizedWeatherData {
+public class Forecast16WeatherData {
 
-    public static final String JSON_F16_WIND = "speed";
+    public static final String JSON_WIND = "speed";
+    public static final String JSON_DATETIME = "dt";
+    public static final String JSON_PRESSURE = "pressure";
+    public static final String JSON_HUMIDITY = "humidity";
 
     private Temperature temp;
     private float pressure;
     private float humidity;
     private float windSpeed;
+    private long dateTime;
 
     public Forecast16WeatherData(JSONObject json) {
-        super(json);
-
-        this.pressure = (float) json.optDouble(Main.JSON_PRESSURE);
-        this.humidity = (float) json.optDouble(Main.JSON_HUMIDITY);
-        this.windSpeed = (float) json
-                .optDouble(Forecast16WeatherData.JSON_F16_WIND);
-
+        this.pressure = (float) json.optDouble(JSON_PRESSURE);
+        this.humidity = (float) json.optDouble(JSON_HUMIDITY);
+        this.windSpeed = (float) json.optDouble(JSON_WIND);
+        this.dateTime = (long) json.optDouble(JSON_DATETIME);
         JSONObject jsonTemp = json.optJSONObject("temp");
         if (jsonTemp != null) {
             this.temp = new Temperature(jsonTemp);
@@ -32,15 +32,26 @@ public class Forecast16WeatherData extends LocalizedWeatherData {
         public static final String JSON_F16_TEMP_DAY = "day";
         public static final String JSON_F16_TEMP_MIN = "min";
         public static final String JSON_F16_TEMP_MAX = "max";
+        public static final String JSON_F16_TEMP_EVE = "night";
+        public static final String JSON_F16_TEMP_NIGHT = "eve";
+        public static final String JSON_F16_TEMP_MORNING = "morn";
 
         private final float day;
         private final float min;
         private final float max;
+        private final float night;
+        private final float eve;
+        private final float morn;
 
         public Temperature(JSONObject json) {
             this.day = (float) json.optDouble(Temperature.JSON_F16_TEMP_DAY);
             this.min = (float) json.optDouble(Temperature.JSON_F16_TEMP_MIN);
             this.max = (float) json.optDouble(Temperature.JSON_F16_TEMP_MAX);
+            this.night = (float) json
+                    .optDouble(Temperature.JSON_F16_TEMP_NIGHT);
+            this.eve = (float) json.optDouble(Temperature.JSON_F16_TEMP_EVE);
+            this.morn = (float) json
+                    .optDouble(Temperature.JSON_F16_TEMP_MORNING);
         }
 
         public float getDay() {
@@ -54,6 +65,18 @@ public class Forecast16WeatherData extends LocalizedWeatherData {
         public float getMax() {
             return max;
         }
+
+        public float getNight() {
+            return night;
+        }
+
+        public float getEve() {
+            return eve;
+        }
+
+        public float getMorn() {
+            return morn;
+        }
     }
 
     public boolean hasTemp() {
@@ -64,16 +87,19 @@ public class Forecast16WeatherData extends LocalizedWeatherData {
         return this.temp;
     }
 
-    public float getTempDay() {
-        return this.temp.getDay();
+    public float getPressure() {
+        return pressure;
     }
 
-    public float getTempMax() {
-        return this.temp.getMax();
+    public float getHumidity() {
+        return humidity;
     }
 
-    public float getTempMin() {
-        return this.temp.getMin();
+    public float getWindSpeed() {
+        return windSpeed;
     }
-
+    
+    public long getDateTime(){
+        return dateTime;
+    }
 }
